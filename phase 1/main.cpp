e2e0aa85c017d33c589998577af634f97286a668
Ritwik <23b0954@iitb.ac.in>
2024-11-18 22:11:42 +0530

    phase-2

--- phase 1/main.cpp
+++ phase 1/main.cpp
@@ -18,6 +18,7 @@ double execute_and_verify_testcase(std::string test_dir) {
 //     for(int i = 0 ; i < submission2.size(); i ++){
 //         std::cout << submission2[i] << " ";
 //     }
+//     std::cout << std::endl << std::endl << std::endl;
 
     std::array<int, 5> output = match_submissions(submission1, submission2);
 
@@ -29,12 +30,23 @@ double execute_and_verify_testcase(std::string test_dir) {
 
     std::array<double, 5> results;
     results[0] = (output[0] == expected[0]) ? 1.0 : 0.0;
+
+//     std::cout << output[0] << "Expectd:" << expected[0] << std::endl;
+
     results[1] = (1.0 * std::min(output[1], expected[1])) / 
             std::max(output[1], expected[1]);
+        //     std::cout << "------------pattern matches detected, of lengths around 10-20----------------"<<std::endl;
+        //     std::cout<<output[1] << " Expected:" << expected[1] <<std::endl;
     results[2] = (1.0 * std::min(output[2], expected[2])) /
             std::max(output[2], expected[2]);
+        //     std::cout << "------------pattern matches detected, of lengths around 30+----------------"<<std::endl;
+        //     std::cout<<output[2] << " Expected:" << expected[2] <<std::endl;
     results[3] = std::pow(1.1, -std::abs(output[3] - expected[3]));
+//     std::cout << "------------starting indices of file1----------------"<<std::endl;
+//     std::cout<<output[3] << " Expected:" << expected[3] <<std::endl;
     results[4] = std::pow(1.1, -std::abs(output[4] - expected[4]));
+//     std::cout << "------------starting indices of file2----------------"<<std::endl;
+//     std::cout<<output[4] << " Expected:" << expected[4] <<std::endl;
     return (2.0 * results[0] + results[1] + results[2] + 
             0.5 * (results[3] + results[4]));
 }
